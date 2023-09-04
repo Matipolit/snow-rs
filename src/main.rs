3e043d74968d83ee183dce5a91e649e43eb9700c
Mateusz Polito <matipolit@gmail.com>
2023-09-04 14:39:06 +0200

    add colorful snow option, add sausage easter egg, change ncurses to crossterm

---
+++ src/main.rs
@@ -0,0 +1,102 @@
+pub(crate) mod snow_particle;
+use crossterm::{
+    cursor,
+    style::Print,
+    terminal, ExecutableCommand, QueueableCommand,
+};
+use rand::Rng;
+use rand::{rngs::ThreadRng, thread_rng};
+use std::io::{self, Write, Stdout};
+use std::{env, thread, time};
+use ctrlc;
+
+
+fn init_particles(
+    particles: &mut Vec<snow_particle::snow_particle::Particle>,
+    amount: i32,
+    max_x: u16,
+    max_y: u16,
+    colorful: bool,
+    rng: &mut ThreadRng,
+    sausage: bool
+) {
+    let sausage_idx = rng.gen_range(0..amount);
+    for i in 0..amount {
+        particles.push(snow_particle::snow_particle::Particle::new(
+            rng.gen_range(0..max_x).into(),
+            rng.gen_range(0..max_y).into(),
+            colorful,
+            i == sausage_idx && sausage,
+            rng
+        ))
+    }
+}
+
+fn print_particles(stdout: &mut Stdout, particles: &Vec<snow_particle::snow_particle::Particle>, debug_info: bool) -> io::Result<()> {
+    let mut i = 0;
+    for particle in particles {
+        particle.print(stdout)?;
+        if debug_info {
+            stdout.queue(cursor::MoveTo(1, i))?
+                .queue(Print(format!("X: {}, Y: {}", particle.x, particle.y).as_str()))?;
+        }
+        i += 1;
+    }
+    stdout.flush()?;
+    Ok(())
+}
+
+fn main() -> io::Result<()> {
+    let mut stdout = io::stdout();
+    let mut delay: time::Duration = time::Duration::from_millis(30);
+    let mut particles: Vec<snow_particle::snow_particle::Particle> = Vec::new();
+    let max_x: u16;
+    let max_y: u16;
+    let mut rng = thread_rng();
+    let args: Vec<String> = env::args().collect();
+    const DEFAULT_PARTICLE_AMOUNT: i32 = 300;
+    let mut particle_amount = DEFAULT_PARTICLE_AMOUNT;
+    
+    if args.contains(&"storm".to_owned()) {
+        particle_amount = 1000;
+    }
+    if args.contains(&"fast".to_owned()) {
+        delay = time::Duration::from_millis(20);
+    }
+    let debug = args.contains(&"debug".to_owned());    
+    let colorful = args.contains(&"colorful".to_owned()) | args.contains(&"color".to_owned());
+    let sausage = args.contains(&"sausage".into());
+    
+    stdout.execute(cursor::Hide)?;    
+    stdout.execute(terminal::Clear(terminal::ClearType::All))?;
+    (max_x, max_y) = terminal::size()?;
+
+    print!("x:{} ", max_x);
+    print!("y: {} ", max_y);
+
+    init_particles(&mut particles, particle_amount, max_x, max_y, colorful, &mut rng, sausage);
+    print_particles(&mut stdout, &particles, debug)?;
+    stdout.flush()?;
+        
+    ctrlc::set_handler(move || {
+        let mut stdout = io::stdout();
+        stdout.execute(terminal::Clear(terminal::ClearType::All)).unwrap();
+        stdout.execute(cursor::Show).unwrap();
+        stdout.execute(cursor::MoveTo(0, 0)).unwrap();
+        std::process::exit(0);
+        }
+        ).unwrap();
+
+    loop {
+        stdout.execute(terminal::Clear(terminal::ClearType::All))?;
+        print_particles(&mut stdout, &particles, debug)?;
+        for particle in &mut particles {
+            particle.float();
+            if particle.y > max_y {
+                particle.respawn(rng.gen_range(0..max_x), 0);
+            }
+        }
+        stdout.flush()?;
+        thread::sleep(delay);
+    }
+}
---
+++ src/snow_particle.rs
@@ -0,0 +1,78 @@
+pub mod snow_particle{
+    use rand::{thread_rng, Rng, rngs::ThreadRng};
+    use std::io::{self, Stdout};
+        use crossterm::{
+        cursor,
+        style::{Stylize, PrintStyledContent, Color, StyledContent},QueueableCommand,
+    };
+    
+    static COLORS: &'static [Color] = &[Color::Black, Color::Blue, Color::Magenta, Color::Green, Color::Yellow];
+
+    pub struct Particle {
+        pub x: u16,
+        pub y: u16,
+        time_since_last_swing: u16,
+        last_swing_direction: bool,
+        swing_time: u16,
+        rng: ThreadRng,
+        content: StyledContent<&'static str>
+    }
+
+    impl Particle {
+        fn get_rand_swing_time(&mut self) -> u16{
+            return self.rng.gen_range(10..20);
+        }
+        pub fn new(beg_x: u16, beg_y: u16, colorful: bool, is_sausage: bool, rng: &mut ThreadRng) -> Particle {
+            let color: Color;
+            if !colorful{
+                color = Color::White;
+            }else{
+                color = COLORS[rng.gen_range(0..COLORS.len())];
+            }
+            let content: StyledContent<&'static str>;
+            if is_sausage{
+                content = "🌭".stylize();
+            }else{
+                content = "*".with(color);
+            }
+            
+            let dir = rng.gen_range(0..=1)==0;
+            let swing_time = rng.gen_range(10..20);
+            
+            Particle{x: beg_x, y: beg_y, time_since_last_swing: 0,
+                last_swing_direction: dir, swing_time, rng: thread_rng(), content }
+        }
+        
+        pub fn float(&mut self) {
+            self.y+=1;
+            if self.last_swing_direction{
+                self.x += 1;
+            }else{
+                if self.x > 0{
+                    self.x -=1;
+                }else{
+                    self.x = 0;
+                }
+
+            }
+            self.time_since_last_swing+=1;
+            if self.time_since_last_swing > self.swing_time {
+                self.last_swing_direction = !self.last_swing_direction;
+                self.time_since_last_swing = 0;
+                self.swing_time = self.get_rand_swing_time();
+            }
+        }
+        
+        pub fn respawn(&mut self, beg_x: u16, beg_y: u16) {
+            self.x = beg_x.into();
+            self.y = beg_y.into();
+            self.time_since_last_swing = 0;
+        }
+        
+        pub fn print(& self,stdout: &mut Stdout) -> io::Result<()> {
+            stdout.queue(cursor::MoveTo(self.x, self.y))?
+                .queue(PrintStyledContent(self.content))?;
+            Ok(())
+        }
+    }
+}
